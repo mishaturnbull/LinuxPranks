681efa852c339ff6259991236069933a7c2a075e
Misha Turnbull <mishaturnbull@gmail.com>
2019-01-24 13:36:21 -0600

    Add rm -rf scare script
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

---
+++ scripts/rmrf_scare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+touch ~/runme.sh
+
+echo "#!/bin/bash" >> ~/runme.sh
+echo "trap '' 2" >> ~/runme.sh
+echo "echo 'Deleting:'" >> ~/runme.sh
+echo "du -h --max-depth=1 --exclude='.*' ~" >> ~/runme.sh
+echo "sleep 3" >> ~/runme.sh
+echo "ls -lahR ~" >> ~/runme.sh
+echo "echo '[1] +  Done                     /bin/rm -rf ~ &'" >> ~/runme.sh
+echo "trap 2" >> ~/runme.sh
+
+echo "Done!"
