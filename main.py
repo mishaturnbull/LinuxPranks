ee2a1eff8a04c1275c72cb0481f67df190658ce5
Misha Turnbull <mishaturnbull@gmail.com>
2019-01-29 19:37:18 -0600

    Move main to root, platform-dependant
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

--- main.py
+++ main.py
@@ -3,22 +3,25 @@
 
 import os
 import sys
+import platform
 
 # load data
-with open('script_db.txt', 'r') as database:
-    lines = database.readlines()
-data = []
-for line in lines:
-    parts = [s.strip() for s in line.split(',')]
-    parts[3] = parts[3].startswith('T')  # True/False
-    data.append(parts)
-print(data)
+def load_data():
+    prefix = platform.system()
+    with open('{}Pranks/script_db.txt'.format(prefix), 'r') as database:
+        lines = database.readlines()
+    data = []
+    for line in lines:
+        parts = [s.strip() for s in line.split(',')]
+        parts[3] = parts[3].startswith('T')  # True/False
+        data.append(parts)
+    return prefix, data
 
 def is_root():
     """Returns true if root, False otherwise."""
     return os.geteuid() == 0
 
-def run_script(name):
+def run_script(name, data):
     """Given a name, run the script after confirmation"""
     for script in data:
         if script[0] == name:
@@ -33,13 +36,13 @@ def run_script(name):
             if inp == 'y':
                 os.system(script[1])
 
-def print_list():
+def print_list(data):
     """Print a list of all the options"""
-    print("#\tScript\tH/D\tRequires sudo?")
+    print("#\tScript\tH/D\tRequires sudo/admin?")
     print("--------------------------------------")
     i = 0
     for script in data:
-        print("{} {}\t{}\t{}".format(
+        print("{} {}\t\t{}\t{}".format(
             str(i),
             script[0],
             script[2],
@@ -47,22 +50,24 @@ def print_list():
         ))
         i += 1
 
-def pick_script():
+def pick_script(data):
     """Get a script name that user wants to execute"""
-    print_list()
+    print_list(data)
     print("Enter a script # > ", sep='')
     inp = input().strip()
     try:
         selection = data[int(inp)]
         return selection[0]
     except:
-        print("You dumb")
-        return pick_script()
+        print("Nope, try again")
+        return pick_script(data)
 
 if __name__ == '__main__':
+    prefix, data = load_data()
     running = True
     while running:
         try:
-            run_script(pick_script())
+            run_script(pick_script(data), data)
         except KeyboardInterrupt as exc:
             running = False
+
